52fb638f49fe4e0eae1916db4521b96d398e496c
joel.navarro@ucb.edu.bo <joel.navarro@ucb.edu.bo>
2024-08-08 00:38:53 -0400

    Estructura basica primer videojuego

--- Pygame1.py
+++ Pygame1.py
@@ -7,3 +7,8 @@ Pantalla = pygame.display.set_mode((500,400))
 
 pygame.display.set_caption("Mi primer juego")
 
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
